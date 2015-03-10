96d717e98904aed0b92900c6ed63c90df7cf3b5d
rahjah1 <roger_2009@ymail.com>
2015-03-10 01:46:15 -0500

    First commit

---
+++ main/java/mycompany/googlegeocoding/GeocodeTalking.java
@@ -0,0 +1,124 @@
+package mycompany.googlegeocoding;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+
+public class GeocodeTalking {
+    String searchString="", newString;
+    String ParsedString;
+    JSONObject ReturnInfo;
+    LatLng newLocation;
+    public GoogleMap Map2;
+    public GeocodeTalking(String temp, GoogleMap Map){
+        searchString=temp;
+        Map2=Map;
+        CallGeocoding();
+    }
+    public void CallGeocoding(){
+        new HttpAsyncTask().execute(generateURL());
+        //Open server, call http provided by generateURL
+    }
+    public String generateURL(){
+        String URLrequest = "https://maps.googleapis.com/maps/api/geocode/json?address=";
+        newString=searchString.replace(" ","+");
+        URLrequest+=newString;
+        return URLrequest;
+    }
+    private class HttpAsyncTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+            return TryHttp(urls[0]);
+        }
+        // onPostExecute displays the results of the AsyncTask.
+        @Override
+        protected void onPostExecute(String result) {
+            try {
+                ParsedString=result;
+                ReturnInfo = new JSONObject(result);
+                JSONObject Location=ReturnInfo.getJSONArray("results").getJSONObject(0)
+                        .getJSONObject("geometry").getJSONObject("location");
+                newLocation = new LatLng(Location.getDouble("lat"),Location.getDouble("lng"));
+                System.out.println(newLocation);
+                MoveMap();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            catch(Exception e ){
+                System.out.println(e);
+            }
+        }
+    }
+
+    public static String TryHttp(String url){
+        InputStream inputStream = null;
+        String result="";
+        try {
+
+            // create HttpClient
+            HttpClient httpclient = new DefaultHttpClient();
+
+            // make GET request to the given URL
+            HttpResponse httpResponse = httpclient.execute(new HttpGet(url));
+
+            // receive response as inputStream
+            inputStream = httpResponse.getEntity().getContent();
+
+            // convert inputstream to string
+            if(inputStream != null)
+                result = buildJSON(inputStream);
+            else
+                result = "Did not work!";
+
+        } catch (Exception e) {
+            Log.d("InputStream", e.getLocalizedMessage());
+        }
+
+        return result;
+    }
+
+    private static String buildJSON(InputStream inputStream) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader( new InputStreamReader(inputStream));
+        StringBuilder build=new StringBuilder();
+        String line = "";
+        String result = "";
+        while((line = bufferedReader.readLine()) != null)
+            result += line;
+        inputStream.close();
+        return result;
+
+    }
+      public void MoveMap() {
+        try {
+            System.out.println("New marker at "+newLocation);
+            Map2.addMarker(new MarkerOptions()
+                    .position(newLocation)
+                    .title("University of Texas")
+                    .draggable(true));
+            System.out.println("Move camera");
+            Map2.moveCamera(CameraUpdateFactory.newLatLngZoom(newLocation, 15));
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
+    }
+}
---
+++ main/java/mycompany/googlegeocoding/MainActivity.java
@@ -0,0 +1,83 @@
+package mycompany.googlegeocoding;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.SearchView;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.inputmethod.InputMethodManager;
+
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import org.json.JSONObject;
+
+public class MainActivity extends ActionBarActivity implements OnMapReadyCallback{
+    MapFragment mapFragment;
+    GoogleMap Map;
+
+    GeocodeTalking g;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mapFragment = (MapFragment) getFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        Map = mapFragment.getMap();
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_activity_actions, menu);
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+        final SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
+        searchView.setQueryHint("Address, City, State");
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener(){
+            @Override
+            public boolean onQueryTextChange(String temp){
+                return false;
+            }
+            @Override
+            public boolean onQueryTextSubmit(String temp){
+                try {
+                    if (temp.length() != 0) {
+                        System.out.println("Try: "+temp);
+                        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                        imm.hideSoftInputFromWindow(searchView.getApplicationWindowToken(), 0);
+                        System.out.println("Sending: "+temp);
+                        g=new GeocodeTalking(temp,Map);
+                        return true;
+                    }
+                }
+                catch (Exception e){
+                    System.out.println(e);
+                }
+                return false;
+            }
+        });
+        return super.onCreateOptionsMenu(menu);
+    }
+    @Override
+    public void onMapReady(GoogleMap map) {
+        LatLng new2Location=new LatLng(30.285117,-97.734100);
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(new2Location, 15));
+        Marker ut =map.addMarker(new MarkerOptions()
+                .position(new2Location)
+                .title("University of Texas")
+                .draggable(true));
+        UiSettings i = map.getUiSettings();
+        i.setZoomControlsEnabled(true);
+        i.setMyLocationButtonEnabled(true);
+    }
+
+}
